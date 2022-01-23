5201eef1b734b8b0b176f9ebc9f77f70fd519b3a
Suhail Qureshi <93420321+Suhail-007@users.noreply.github.com>
2022-01-23 18:30:16 +0530

    Add files via upload
    Added javascript animation
    Fixed glitch when going forward and backward

--- Script/script.js
+++ Script/script.js
@@ -1,21 +1,25 @@
 const buttons = document.querySelectorAll('[data-carousel-btn]');
 
+//Buttons
 buttons.forEach(function(button) {
 		button.addEventListener('click', () => {
 
 			let offset = button.dataset.carouselBtn === 'next' ? 1 : -1;
-			
+	
 			let slides = button.closest('[data-carousel]')
 				.querySelector('[data-slides]');
 				
 			const activeSlide = document.querySelector('[data-active]');
-	
+
 	  	let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 		
 	  	if (newIndex < 0) newIndex = slides.children.length-1;
 		
 		 if(newIndex >= slides.children.length) newIndex = 0;
-	
+		 //animaton
+			slides.children[newIndex].style.transition = '300ms opacity ease-in-out'
+			activeSlide.style.transitionDelay = '0ms'
+			
 		 slides.children[newIndex].dataset.active = true;
    delete activeSlide.dataset.active;
 		})
